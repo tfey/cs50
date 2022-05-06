2cf5a72d1c5d001566673a1a4599ee16027914ec
tfey <thorstenfey@outlook.com>
2022-05-06 22:08:38 +0200

    initial upload

--- cs50python/UnitTests/test_calculator.py
+++ cs50python/UnitTests/test_calculator.py
@@ -1,14 +1,15 @@
 from calculator import square
 
 
-def main():
-    test_square()
-
-
 def test_square():
     assert square(2) == 4
     assert square(3) == 9
 
 
-if __name__ == "__main__":
-    main()
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+
+def test_zero():
+    assert square(0) == 0
