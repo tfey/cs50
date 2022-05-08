84ec44d9ce1df4e81a88073e07a9587fe05a7c37
tfey <thorstenfey@outlook.com>
2022-05-08 10:54:18 +0200

    initial upload

---
+++ cs50python/RegularExpressions/format.py
@@ -0,0 +1,6 @@
+import re
+
+name = input("What's your name? ").strip()
+if matches := re.search("^(.+), *(.+)$", name):
+    name = matches.group(2) + " " + matches.group(1)
+print(f"hello, {name}")
